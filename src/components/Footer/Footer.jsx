f701fa0b275aacfab2100a44e9f66b4da69460f6
Mariam-Hesham88 <mariam.hesham.ramadan@gmail.com>
2025-11-08 11:31:54 +0200

    Added link at footer components

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return <>
   <section className='py-5 bg-primary text-white text-center'>
     <h3 className={style.font}>Developed By: Mariam Hesham Ramadan</h3>
-    <h4 className={style.font}>Designed By: Doha Taher</h4>
+    <h4 className={style.font}>Designed By: <a href="https://www.linkedin.com/in/doha-taher-5a9b3231b/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target='_blank' className='underline'>Doha Taher</a></h4>
   </section>
   </>
 }
